bbc526b976342a7985ae038ddbd12b8f462b68c6
Dev Damian <damian.szmigielski.it@gmail.com>
2022-04-15 17:33:11 +0200

    Modified 15.04.2022 g.17:30

--- src/dev/damian_szmigielski/MetodyTeoria.java
+++ src/dev/damian_szmigielski/MetodyTeoria.java
@@ -1,5 +1,34 @@
 package dev.damian_szmigielski;
 
+
 public class MetodyTeoria {
-    public void  
+    //public - definiujemy metode, korzystamy z identyfikatora dostepu
+    //void - podajemy zwracany typ danych int/string/Task5Student/ void oznacza, ze nie zwraca zadnej wartosci
+    //nazwa metody - z malej litery
+    //lekcja 81/82
+    public int pobierzWynik(){
+        System.out.println("Zaraz policze wynik");
+        int result = 0;
+        for (int i=0; i < 100; i++){
+            result = result + i;
+        }
+        System.out.println("Rezultat to: " + result);
+        return result;
+    }
+
+    //lekcja 83 parametry metod
+    public int policzWynikParam(int number){
+        System.out.println("Number ma wartosc: " + number);
+        System.out.println("Zaraz policze wynik");
+        int result = 0;
+        for (int i=0; i < number; i++){
+            result = result + i;
+        }
+        System.out.println("Rezultat to: " + result);
+        return result;
+    }
+    //83 parametry
+    public void add(int firstNumber, int secondNumber ) {
+        System.out.println("Suma to: " + (firstNumber + secondNumber));
+    }
 }
---
+++ src/dev/damian_szmigielski/MetodyTest.java
@@ -0,0 +1,13 @@
+package dev.damian_szmigielski;
+
+public class MetodyTest {
+    public static void main (String[] args){
+        MetodyTeoria metody = new MetodyTeoria();
+//        metody.pobierzWynik();
+//        int result = metody.pobierzWynik();
+        metody.policzWynikParam(2);
+
+        metody.add(2,3);
+
+    }
+}
