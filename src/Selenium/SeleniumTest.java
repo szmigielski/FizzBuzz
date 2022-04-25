60d151ac74ab210f83d8b3db98b5ddc37d524b62
Dev Damian <damian.szmigielski.it@gmail.com>
2022-04-25 13:55:10 +0200

    Modified 25.04 g.14:00

---
+++ src/Selenium/SeleniumTest.java
@@ -0,0 +1,10 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args){
+        WebDriver driver = new FirefoxDriver()
+    }
+}
--- src/interview/Fibonacci.java
+++ src/interview/Fibonacci.java
@@ -11,12 +11,14 @@ public class Fibonacci {
     }
 
         public static void fibo ( int n){
-            long n1 = 1;
-            long n2 = 2;
+            long n1 = 1; //pierwszy wyraz ciągu
+            long n2 = 2; //drugi wyraz ciagu
 
+
+            //i=3, bo znamy 1 i 2 wyraz
             for (int i = 3; i <= n; i++) {
-                long temp = n2 + n1;
-                n1 = n2;
+                long temp = n2 + n1; //suma dwóch poprzednich wyrazów
+                n1 = n2;             //nadpisuje n1, ustawiam mu wartość n2
                 n2 = temp;
             }
             System.out.println(n2);
